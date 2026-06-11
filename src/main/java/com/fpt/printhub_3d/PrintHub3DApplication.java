b11d334cea8e4991ea9bb103066c82447353c90e
khanh <khanhle0209c@gmail.com>
2026-06-11 08:04:18 +0700

    [INIT] first commit

---
+++ src/main/java/com/fpt/printhub_3d/PrintHub3DApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.printhub_3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintHub3DApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintHub3DApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/printhub_3d/PrintHub3DApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.printhub_3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintHub3DApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
